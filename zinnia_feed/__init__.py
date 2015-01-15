123d516a5dbc41d0a1e4aa7243fb7980ea878750
Fantomas42 <fantomas42@gmail.com>
2015-01-15 12:09:52 +0100

    Create zinnia_feed package

---
+++ zinnia_feed/__init__.py
@@ -0,0 +1,8 @@
+"""feed2zinnia"""
+__version__ = '1.0'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/django-blog-zinnia/feed2zinnia'
